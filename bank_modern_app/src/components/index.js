0b92481af7f56be6e09aa8a68d66d05f11f84842
Eduardo.Lopes <eduardo.lopes-ext@telecall.com>
2023-05-09 17:02:15 -0300

    estrutura inicial do projeto bank app

---
+++ bank_modern_app/src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar'
+import Stats from './components/Stats'
+import Business from './components/Business'
+import Billing from './components/Billing'
+import CardDeal from './components/CardDeal'
+import Testimonials from './components/Testimonials'
+import Clients from './components/Clients'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
+import FeedbackCard from './components/FeedbackCard'
+import Hero from './components/Hero'
+
+export {
+    Navbar,
+    Stats,
+    Business,
+    Billing,
+    CardDeal,
+    Testimonials,
+    Clients,
+    CTA,
+    Footer,
+    FeedbackCard,
+    Hero,
+};
